042c722ded2eb45b38220baed502e12ef8d2eb1e
leonardo <leonardo.scommegna@stud.unifi.it>
2019-07-14 11:30:24 +0200

    Refactor of the test code
    First TDD cycle ended with a refactor of the testAllCourses() method

--- schoolmanager-frontend/src/test/java/apt/project/frontend/controller/CourseControllerTest.java
+++ schoolmanager-frontend/src/test/java/apt/project/frontend/controller/CourseControllerTest.java
@@ -7,7 +7,11 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import apt.project.backend.domain.Course;
 import apt.project.backend.repository.CourseRepository;
@@ -15,19 +19,24 @@ import apt.project.frontend.view.View;
 
 public class CourseControllerTest {
 
-    
+    @Mock
+    private CourseRepository courseRepository;
 
+    @Mock
+    private View courseView;
+
+    @InjectMocks
+    private CourseController courseController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testAllCourses() {
-        // setup
-        // TODO refactor
-        CourseRepository courseRepository = mock(CourseRepository.class);
-        View courseView = mock(View.class);
-        CourseController courseController = new CourseController(courseView, courseRepository);
         List<Course> courses = asList(new Course());
-        when(courseRepository.findAll())
-        .thenReturn(courses);
+        when(courseRepository.findAll()).thenReturn(courses);
         // exercise
         courseController.allCourses();
         // verify
