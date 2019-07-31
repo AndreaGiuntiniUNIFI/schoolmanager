4a9bdc45b087ccb4ef5533dfe3d458c2c2994c2c
leonardo <leonardo.scommegna@stud.unifi.it>
2019-07-31 18:10:17 +0200

    Implemented add course dialog when add button is pressed

--- schoolmanager-frontend/src/main/java/apt/project/frontend/view/swing/CoursePanel.java
+++ schoolmanager-frontend/src/main/java/apt/project/frontend/view/swing/CoursePanel.java
@@ -1,5 +1,7 @@
 package apt.project.frontend.view.swing;
 
+import static java.util.Arrays.asList;
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -8,12 +10,16 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import apt.project.backend.domain.Course;
 import apt.project.frontend.view.View;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class CoursePanel extends JPanel implements View<Course> {
 
@@ -27,12 +33,13 @@ public class CoursePanel extends JPanel implements View<Course> {
      */
     public CoursePanel() {
         GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagLayout.columnWidths = new int[]{0, 0, 0, 0};
-        gridBagLayout.rowHeights = new int[]{0, 0, 0};
-        gridBagLayout.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
-        gridBagLayout.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+        gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0 };
+        gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
+        gridBagLayout.columnWeights = new double[] { 1.0, 0.0, 0.0,
+                Double.MIN_VALUE };
+        gridBagLayout.rowWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
         setLayout(gridBagLayout);
-        
+
         JScrollPane scrollPane = new JScrollPane();
         GridBagConstraints gbc_scrollPane = new GridBagConstraints();
         gbc_scrollPane.fill = GridBagConstraints.BOTH;
@@ -41,23 +48,29 @@ public class CoursePanel extends JPanel implements View<Course> {
         gbc_scrollPane.gridx = 0;
         gbc_scrollPane.gridy = 0;
         add(scrollPane, gbc_scrollPane);
-        
+
         JList list = new JList();
         list.setName("coursesList");
         scrollPane.setViewportView(list);
-        
+
         JLabel lblListOfCourses = new JLabel("List of Courses");
         lblListOfCourses.setHorizontalAlignment(SwingConstants.CENTER);
         scrollPane.setColumnHeaderView(lblListOfCourses);
-        
+
         JButton btnAdd = new JButton("Add");
-        
+        btnAdd.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                getCustomInputDialog("Title", "Add Course");
+            }
+        });
+
         GridBagConstraints gbc_btnAdd = new GridBagConstraints();
         gbc_btnAdd.insets = new Insets(0, 0, 0, 5);
         gbc_btnAdd.gridx = 0;
         gbc_btnAdd.gridy = 1;
         add(btnAdd, gbc_btnAdd);
-        
+
         JButton btnModify = new JButton("Modify");
         btnModify.setEnabled(false);
         GridBagConstraints gbc_btnModify = new GridBagConstraints();
@@ -65,7 +78,7 @@ public class CoursePanel extends JPanel implements View<Course> {
         gbc_btnModify.gridx = 1;
         gbc_btnModify.gridy = 1;
         add(btnModify, gbc_btnModify);
-        
+
         JButton btnDelete = new JButton("Delete");
         btnDelete.setEnabled(false);
         GridBagConstraints gbc_btnDelete = new GridBagConstraints();
@@ -75,34 +88,59 @@ public class CoursePanel extends JPanel implements View<Course> {
 
     }
 
+    public String getCustomInputDialog(String field, String title) {
+
+        JPanel panel = new JPanel();
+
+        JLabel label = new JLabel(field);
+        panel.add(label);
+
+        JTextField textField = new JTextField(10);
+        panel.add(textField);
+
+        JButton okBtn = new JButton("OK");
+        okBtn.setEnabled(false);
+        panel.add(okBtn);
+
+        JButton cancelBtn = new JButton("Cancel");
+        panel.add(cancelBtn);
+
+        int choice = JOptionPane.showOptionDialog(null, panel, title,
+                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null,
+                new JButton[] { okBtn, cancelBtn }, okBtn);
+
+        return " ";
+
+    }
+
     @Override
     public void showAll(List<Course> entity) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void entityAdded(Course entity) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void showError(String string, Course entity) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void entityDeleted(Course entity) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void entityUpdated(Course existingEntity, Course modifiedEntity) {
         // TODO Auto-generated method stub
-        
+
     }
 
 }
--- schoolmanager-frontend/src/test/java/apt/project/frontend/view/swing/CoursePanelTest.java
+++ schoolmanager-frontend/src/test/java/apt/project/frontend/view/swing/CoursePanelTest.java
@@ -1,11 +1,14 @@
 package apt.project.frontend.view.swing;
 
+import static org.assertj.swing.timing.Timeout.timeout;
+
 import javax.swing.JFrame;
 
 import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.core.matcher.JLabelMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.JOptionPaneFixture;
 import org.assertj.swing.fixture.JPanelFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
@@ -31,6 +34,7 @@ public class CoursePanelTest extends AssertJSwingJUnitTestCase {
         GuiActionRunner.execute(() -> {
             jframe = new JFrame();
             jframe.getContentPane().add(coursePanel);
+            jframe.pack();
             jframe.setVisible(true);
             return jframe;
         });
@@ -50,6 +54,16 @@ public class CoursePanelTest extends AssertJSwingJUnitTestCase {
         panelFixture.button(JButtonMatcher.withText("Modify"))
                 .requireDisabled();
         panelFixture.list("coursesList");
+    }
+    
+    @Test
+    @GUITest
+    public void TestWhenAddButtonIsPressedThenShowEmptyDialog() {
+        panelFixture.button(JButtonMatcher.withText("Add")).click();
+        JOptionPaneFixture optionFixture = panelFixture.optionPane(timeout(1000));
+        optionFixture.button(JButtonMatcher.withText("OK")).requireDisabled();
+        optionFixture.button(JButtonMatcher.withText("Cancel")).requireEnabled();
+
     }
 
 }
